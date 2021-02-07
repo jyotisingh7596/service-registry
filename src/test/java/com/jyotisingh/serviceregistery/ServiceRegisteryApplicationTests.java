3c3daeea82477ccfc517b3bcc17f66c4409dfac5
jyotisingh7596 <75801620+jyotisingh7596@users.noreply.github.com>
2021-02-07 13:43:49 +0530

    Initial commit

---
+++ src/test/java/com/jyotisingh/serviceregistery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyotisingh.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
